4a95b938f07f87e796ef8234e937516e7b2b5e62
29Anirudh <anirudhbatana@gmail.com>
2025-05-30 17:08:43 +0530

    Deploy now

--- server.js
+++ server.js
@@ -26,14 +26,5 @@ app.use("/api/auth", authRoutes);
 app.use("/api/blogs", blogRoutes);
 
 // Connect to MongoDB and start server
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.error("MongoDB connection error:", err));
+mongoose.connect(MONGO_URI);
 //This is the main server or app or index.js
